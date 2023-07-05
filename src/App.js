c242754fa95436eaadf4a2ef45173c18209a6845
unknown <smeena1@es.iitr.ac.in>
2023-07-05 15:46:26 +0530

    hooks and state experiments

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-// import logo from './logo.svg';
-// import './App.css';
+// import reac,{ useState } from "react";
+// // import handelClick from "handelClick";
 
-import {useState} from "react" ;
+
+
+//     handelClick() {
+      
+//        setCount(count+1);
+//       };
+
+
+//       function App() {
+//       functionconst[count,setCount] = useState(0);
+        
+//      return (
+      
+//     <div className="App">
+//       <button click = 'handelClick'> Clicked me{count}</button> // it will reflect changes diff. diff on each call & click
+
+//       <br></br>
+//       <button click = 'handelClick'> Clicked me{count}</button> 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 import MyButton from "./MyButton";
 
 function App() {
-  const [Data, setData] = useState(0)
+  const [count, setCount] = useState(0);
 
-    function counting_change()
-    {
-      setData(Data+1);
-      if(Data >=10)
-      {
-        setData(Data-10);
-      }
-    }
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-
     <div className="App">
-      <h1>{Data}</h1>
-      <button onClick={counting_change}>Click Me</button>
-      <MyButton></MyButton> // it will reflect changes diff. diff on each call & click
-
-      <br></br>
+      <span> <h2>These buttuns update by sigle click  </h2> </span>
+      <button onClick={handleClick}>Clicked me {count}</button>
+      
+      <button onClick={handleClick}>Clicked me {count}</button>
+      <br />
+      <h2>Now these two button sahre same component but they keeps there own vaue</h2>
+      <MyButton></MyButton>
       <MyButton></MyButton>
     </div>
   );
